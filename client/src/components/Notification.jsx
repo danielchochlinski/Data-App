9e013c2e7447f11b5ab5b81a9ac86a5318435f9b
danielchochlinski <danielchochlinski@hotmail.com>
2022-07-26 11:47:13 +0200

    not time change

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -9,7 +9,7 @@ const Notification = (props) => {
     const id = setInterval(() => {
       setWidth((prev) => {
         if (prev < 100) {
-          return prev + 0.5;
+          return prev + 1;
         }
 
         clearInterval(id);
