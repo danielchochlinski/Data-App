004561a3a467ddfbaa3f25d1d8729963b8ece299
danielchochlinski <danielchochlinski@hotmail.com>
2022-07-26 15:08:19 +0200

    prod string

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,16 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-
 app.use("/api/data", require("./routes/databaseRouter"));
 
 //serve frontend
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
+  );
 } else {
   app.get("/", (req, res) => res.send("Set production environment"));
 }
--- client/src/view/Main.jsx
+++ client/src/view/Main.jsx
@@ -4,7 +4,7 @@ import List from "../components/List";
 import SearchedDataModal from "../components/SearchedDataModal";
 import { useNotification } from "../context/NotificationProvider";
 import { uniqueID } from "../utils/functions";
-
+const URL_PROD = "https://device-data-app.herokuapp.com/api/data"
 const URL = "http://localhost:5000/api/data";
 const dataLocalStorage = JSON.parse(localStorage.getItem("history") || "[]");
 const Main = () => {
@@ -15,7 +15,7 @@ const Main = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    const response = await axios.get(`${URL}/${value}`);
+    const response = await axios.get(`${URL_PROD}/${value}`);
 
     if (response.data.message) {
       dispatch({
